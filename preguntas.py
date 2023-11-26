0fb0625bc81904c25735da551ce994e2d33ff856
salperezate <salperezate@unal.edu.co>
2023-11-26 15:48:53 -0500

    pregunta 3

--- preguntas.py
+++ preguntas.py
@@ -180,12 +180,6 @@ def pregunta_02():
 
     # Divida los datos de entrenamiento y prueba. La semilla del generador de números
     # aleatorios es 123. Use 100 patrones para la muestra de prueba.
-    # (X_train, X_test, y_train, y_test,) = ____(
-    #     ____,
-    #     ____,
-    #     test_size=____,
-    #     random_state=____,
-    # )
     X_train, X_test, y_train, y_test = train_test_split(
         X,
         y,
@@ -203,10 +197,13 @@ def pregunta_03():
     """
 
     # Importe ColumnTransformer
+    from sklearn.compose import make_column_transformer
     # Importe SVC
+    from sklearn.svm import SVC
     # Importe OneHotEncoder
+    from sklearn.preprocessing import OneHotEncoder
     # Importe Pipeline
-    from ____ import ____
+    from sklearn.pipeline import Pipeline
 
     # Cargue las variables.
     X_train, _, y_train, _ = pregunta_02()
@@ -214,24 +211,38 @@ def pregunta_03():
     # Cree un objeto ColumnTransformer que aplique OneHotEncoder a las columnas
     # tipo texto. Use make_column_selector para seleccionar las columnas. Las
     # columnas numéricas no deben ser transformadas.
+    # columnTransformer = make_column_transformer(
+    #     (
+    #         ____(),
+    #         ____(____=____),
+    #     ),
+    #     remainder=____,
+    # )
     columnTransformer = make_column_transformer(
         (
-            ____(),
-            ____(____=____),
+            OneHotEncoder(),
+            make_column_selector(dtype_include=object),
         ),
-        remainder=____,
+        remainder="passthrough",
     )
 
     # Cree un pipeline que contenga el columnTransformer y el modelo SVC.
-    pipeline = ____(
+    # pipeline = ____(
+    #     steps=[
+    #         ("____", ____),
+    #         ("____", ____),
+    #     ],
+    # )
+    pipeline = Pipeline(
         steps=[
-            ("____", ____),
-            ("____", ____),
+            ("columnTransformer", columnTransformer),
+            ("SVC", SVC()),
         ],
     )
 
     # Entrene el pipeline con los datos de entrenamiento.
-    ____.____(____, ____)
+    # ____.____(____, ____)
+    pipeline.fit(X_train, y_train)
 
     # # Retorne el pipeline entrenado
     return pipeline
